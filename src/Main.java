b1997acc377fce67f33f53ea5d95dc3a37b38c9b
Justin Hollmer <justin.hollmer@gmail.com>
2022-09-27 15:25:32 -0500

    finished the syntax, types, and variables exercises

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int myFavoriteNumber = 5;
+        System.out.println(myFavoriteNumber);
+        String myString = "Justin";
+        System.out.println(myString);
+        long myNumber;
+        myNumber = 123;
+        System.out.println(myNumber);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+
+
+
+
+
+
     }
-
-
-
-
-
-
-
-
-
 }
