642600db872254e406f16a30486ef9b34512f449
Justin Hollmer <justin.hollmer@gmail.com>
2022-09-28 16:52:28 -0500

    Completed exercise 4

--- src/ControFlowExercises.java
+++ src/ControFlowExercises.java
@@ -3,51 +3,28 @@ import java.util.Scanner;
 public class ControFlowExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Going to enter a table of numbers, what would you like the table to go up to: ");
-        int userInput = scanner.nextInt();
+        System.out.print("Would you like to enter your grade here? (y/n): ");
+        String userInput = scanner.next();
         scanner.nextLine();
-        System.out.println();
-        System.out.println("Here is your table!");
-        System.out.println();
-        System.out.println("Number | Squared | Cubed");
-        System.out.println("------ | ------- | -----");
+        if (userInput.equals("y")) {
 
-        for (int i = 1; i <= userInput; i++) {
-            String numberString = "";
-            String squaredString = "";
-            int cubedString = i*i*i;
-            int numberField = i;
-            int squaredField = i*i;
-            int cubedField = i*i*i;
-            if (numberField < 10) {
-                numberString = numberField+"      | ";
+            System.out.print("Please enter your grade here: ");
+            int userGrade = scanner.nextInt();
+            if (userGrade >= 90 && userGrade <= 100) {
+                System.out.println("Congratulation! You got an A!");
             }
-            else if (numberField < 100) {
-                numberString = numberField+"     | ";
+            else if (userGrade >= 80 && userGrade <= 89){
+                System.out.println("You got a B, not bad.");
             }
-            if (squaredField < 10) {
-                squaredString = squaredField+"       | ";
+            else if (userGrade >= 70 && userGrade <= 79){
+                System.out.println("You got a c.");
             }
-            else if (squaredField < 100) {
-                squaredString = squaredField+"      | ";
+            else {
+                System.out.println("You failed.");
             }
-            else if (squaredField < 1000) {
-                squaredString = squaredField+"     | ";
-            }
-            else if (squaredField < 10000) {
-                squaredString = squaredField+"    | ";
-            }
-            else if (squaredField < 100000) {
-                squaredString = squaredField+"   | ";
-            }
-            else if (squaredField < 1000000) {
-                squaredString = squaredField+"  | ";
-            }
-            else if (squaredField < 10000000) {
-                squaredString = squaredField+" | ";
-            }
-
-            System.out.println(numberString + squaredString + cubedString);
+        }
+        else {
+            System.out.println("Aight then");
         }
 
     }
