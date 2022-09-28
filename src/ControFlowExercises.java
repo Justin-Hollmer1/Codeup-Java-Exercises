6b25f04869e24880d037051812e91b6c509c4b65
Justin Hollmer <justin.hollmer@gmail.com>
2022-09-28 16:55:08 -0500

    Finished the bonus exercise

--- src/ControFlowExercises.java
+++ src/ControFlowExercises.java
@@ -10,14 +10,23 @@ public class ControFlowExercises {
 
             System.out.print("Please enter your grade here: ");
             int userGrade = scanner.nextInt();
-            if (userGrade >= 90 && userGrade <= 100) {
-                System.out.println("Congratulation! You got an A!");
+            if (userGrade >= 95 && userGrade <= 100) {
+                System.out.println("A+");
             }
-            else if (userGrade >= 80 && userGrade <= 89){
-                System.out.println("You got a B, not bad.");
+            else if (userGrade >= 90 && userGrade <= 94){
+                System.out.println("A-");
             }
-            else if (userGrade >= 70 && userGrade <= 79){
-                System.out.println("You got a c.");
+            else if (userGrade >= 85 && userGrade <= 89){
+                System.out.println("B+");
+            }
+            else if (userGrade >= 80 && userGrade <= 84){
+                System.out.println("B-");
+            }
+            else if (userGrade >= 75 && userGrade <= 79){
+                System.out.println("C+");
+            }
+            else if (userGrade >= 70 && userGrade <= 74){
+                System.out.println("C-");
             }
             else {
                 System.out.println("You failed.");
