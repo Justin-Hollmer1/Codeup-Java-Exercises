c1fe76161127d403f9bc5b56965c9d47c13b0dfa
Justin Hollmer <justin.hollmer@gmail.com>
2022-09-28 16:42:31 -0500

    Exercise 3 complete

--- src/ControFlowExercises.java
+++ src/ControFlowExercises.java
@@ -1,21 +1,54 @@
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class ControFlowExercises {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Going to enter a table of numbers, what would you like the table to go up to: ");
+        int userInput = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+        System.out.println("Here is your table!");
+        System.out.println();
+        System.out.println("Number | Squared | Cubed");
+        System.out.println("------ | ------- | -----");
+
+        for (int i = 1; i <= userInput; i++) {
+            String numberString = "";
+            String squaredString = "";
+            int cubedString = i*i*i;
+            int numberField = i;
+            int squaredField = i*i;
+            int cubedField = i*i*i;
+            if (numberField < 10) {
+                numberString = numberField+"      | ";
             }
-            else if (i % 3 == 0) {
-                System.out.println("Fizz");
+            else if (numberField < 100) {
+                numberString = numberField+"     | ";
             }
-            else if (i % 5 == 0) {
-                System.out.println("Buzz");
+            if (squaredField < 10) {
+                squaredString = squaredField+"       | ";
             }
-            else {
-                System.out.println(i);
+            else if (squaredField < 100) {
+                squaredString = squaredField+"      | ";
             }
+            else if (squaredField < 1000) {
+                squaredString = squaredField+"     | ";
+            }
+            else if (squaredField < 10000) {
+                squaredString = squaredField+"    | ";
+            }
+            else if (squaredField < 100000) {
+                squaredString = squaredField+"   | ";
+            }
+            else if (squaredField < 1000000) {
+                squaredString = squaredField+"  | ";
+            }
+            else if (squaredField < 10000000) {
+                squaredString = squaredField+" | ";
+            }
+
+            System.out.println(numberString + squaredString + cubedString);
         }
+
     }
 }
