edd44187efbaf757cd2d37d9fa802c9f57f3fdeb
Justin Hollmer <justin.hollmer@gmail.com>
2022-10-03 11:39:06 -0500

    Finished exercise 2 and the bonuses

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class MethodsExercises {
     public static void main(String[] args) {
-        recurse();
+//        recurse();
+        doTheThing();
     }
     public static double add(double input1, double input2) {
         return input1 + input2;
@@ -52,4 +53,24 @@ public class MethodsExercises {
             System.exit(0);
         }
     }
+
+
+    public static void doTheThing() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number between 1 and 10: ");
+        int userInput = scanner.nextInt();
+        if (userInput > 10 || userInput < 1) {
+            System.out.println("Invalid input");
+            doTheThing();
+        }
+        else {
+            System.out.println("Thank you");
+            System.exit(0);
+        }
+    }
+
+
+
+
+
 }
