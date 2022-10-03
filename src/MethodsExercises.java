041e190b47d75e817e51e45f403f48691b83fb42
Justin Hollmer <justin.hollmer@gmail.com>
2022-10-03 12:22:28 -0500

    Finished exercise 4

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class MethodsExercises {
     public static void main(String[] args) {
 //        recurse();
 //        doTheThing();
-        factorial();
+//        factorial();
+        diceRoll();
+
     }
     public static double add(double input1, double input2) {
         return input1 + input2;
@@ -25,7 +28,6 @@ public class MethodsExercises {
     public static double modulus(double input1, double input2) {
         return input1 % input2;
     }
-
     public static void recurse() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter 2 numbers to add");
@@ -54,8 +56,6 @@ public class MethodsExercises {
             System.exit(0);
         }
     }
-
-
     public static void doTheThing() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a number between 1 and 10: ");
@@ -69,7 +69,6 @@ public class MethodsExercises {
             System.exit(0);
         }
     }
-
     public static void factorial() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a number between 1 and 20 to get the factorial of that number.");
@@ -90,12 +89,44 @@ public class MethodsExercises {
             System.exit(0);
         }
     }
-
     public static long factorialCalculation(int input) {
         long total = input;
         for (int i = 1; i < input; i++) {
             total *= i;
         }
         return total;
+    }
+    public static void diceRoll() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the nubmer of sides for the dices.");
+        System.out.print("Dice 1 sides: ");
+        int diceSides1 = scanner.nextInt();
+        System.out.print("Dice 2 sides: ");
+        int diceSides2 = scanner.nextInt();
+        System.out.println("Would you like to roll the dice? Enter \"y\" to roll.");
+        scanner.nextLine();
+        String userAnswer = scanner.nextLine();
+        if (userAnswer.equalsIgnoreCase("y")) {
+            System.out.format("Dice roll 1 = %s", theRoll(diceSides1));
+            System.out.println();
+            System.out.format("Dice roll 2 = %s", theRoll(diceSides2));
+            System.out.println();
+        }
+        else {
+            System.exit(0);
+        }
+        System.out.println("Would you like to start again? To go again press \"y\".");
+        String playAgain = scanner.nextLine();
+        if (playAgain.equalsIgnoreCase("y")) {
+            diceRoll();
+        }
+        else {
+            System.exit(0);
+        }
+
+    }
+    public static int theRoll(int input) {
+        int random = (int)((Math.random()*input) + 1);
+        return random;
     }
 }
