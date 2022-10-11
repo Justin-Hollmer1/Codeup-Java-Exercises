b7301dd4a6d4e93973f491be23c9248eaee081bc
Justin Hollmer <justin.hollmer@gmail.com>
2022-10-11 12:17:25 -0500

    Done with some of the bonus problems

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -12,6 +12,12 @@ public class GradesApplication {
         Student CStudent = new Student("Student1", new ArrayList<Integer>());
         Student DStudent = new Student("Student1", new ArrayList<Integer>());
 
+        ArrayList<Student> studentList = new ArrayList<>();
+        studentList.add(AStudent);
+        studentList.add(BStudent);
+        studentList.add(CStudent);
+        studentList.add(DStudent);
+
         AStudent.addGrade(95);
         AStudent.addGrade(96);
         AStudent.addGrade(97);
@@ -43,6 +49,18 @@ public class GradesApplication {
             System.out.println();
             System.out.println("|UsernameForAStudent| |UsernameForBStudent| |UsernameForCStudent| |UsernameForDStudent|");
             System.out.println();
+            System.out.println("Would you like to see the grades for all of the students?");
+            String allGrades = scanner.nextLine();
+            if (allGrades.equalsIgnoreCase("yes") || allGrades.equalsIgnoreCase("y")) {
+                System.out.println("All grades: ");
+                for(int i = 0; i < students.size(); i++) {
+                    System.out.printf("Student %s: %s, Average: %s", i, studentList.get(i).getGrades(), studentList.get(i).getGradeAverage());
+                    System.out.println();
+                }
+            }
+            else {
+
+            }
             System.out.println("What student would you like to see information on?");
             String userInput = scanner.nextLine();
             int testValue = 0;
@@ -60,6 +78,8 @@ public class GradesApplication {
             System.out.printf("Name: %s", students.get(userInput).getName());
             System.out.println();
             System.out.printf("GitHub Username: %s", userInput);
+            System.out.println();
+            System.out.printf("Current grades: %s", students.get(userInput).getGrades());
             System.out.println();
             System.out.printf("Current Grade Average: %s", students.get(userInput).getGradeAverage());
             System.out.println();
