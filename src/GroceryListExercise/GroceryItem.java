a0136bc23e95bf8ee85dbf0cdcc0e71f334df450
Justin Hollmer <justin.hollmer@gmail.com>
2022-10-12 09:36:34 -0500

    Base requirements done for both exercises

--- src/GroceryListExercise/GroceryItem.java
+++ src/GroceryListExercise/GroceryItem.java
@@ -2,10 +2,12 @@ package GroceryListExercise;
 
 public class GroceryItem {
     private String name;
+    private int quantity;
     private String catagory;
 
-    public GroceryItem(String name, String catagory) {
+    public GroceryItem(String name, int quantity, String catagory) {
         this.name = name;
+        this.quantity = quantity;
         this.catagory = catagory;
     }
 
@@ -16,8 +18,14 @@ public class GroceryItem {
     public String getCatagory() {
         return this.catagory;
     }
+    public int getQuantity() {
+        return this.quantity;
+    }
     public void setName(String name) {
         this.name = name;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
     public void setCatagory(String catagory) {
         this.catagory = catagory;
--- src/GroceryListExercise/Run.java
+++ src/GroceryListExercise/Run.java
@@ -1,51 +1,73 @@
 package GroceryListExercise;
 
+import javax.print.MultiDocPrintService;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Run {
     public static void main(String[] args) {
-        //This is the category list
-        ArrayList<String> list = new ArrayList<>();
-        list.add("Meat");
-        list.add("Dairy");
-        list.add("Canned Goods");
-        list.add("Cleaning");
-        list.add("Cookware");
-        list.add("Produce");
-        list.add("Condiments");
-
-        //Initalizing a list for all of the grocery items.
-        HashMap<GroceryItem, Integer> groceryList = new HashMap<GroceryItem, Integer>();
-
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Would you like to enter a grocery list? Please enter \"yes\" to continue.");
-        String userInput = scanner.nextLine();
-        if (userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y")) {
-            System.out.println("Would you like to add a new item?");
-            String userEnterItem = scanner.nextLine();
-            if (userEnterItem.equalsIgnoreCase("yes") || userEnterItem.equalsIgnoreCase("y")) {
-                System.out.printf("Please select the items catagory: %s", list);
-                System.out.println();
-                String userItemType = scanner.nextLine();
-                if (!list.contains(userItemType)) {
-                    System.out.println("Please enter a valid item type.");
+
+        ArrayList<String> categoryList = new ArrayList<String>();
+        categoryList.add("Meat");
+        categoryList.add("Dairy");
+        categoryList.add("Produce");
+        categoryList.add("Cleaning Supplies");
+        categoryList.add("Pet Supplies");
+        categoryList.add("Canned Goods");
+        categoryList.add("Condiments");
+        categoryList.add("Frozen Things");
+        ArrayList<String> groceryItemsList = new ArrayList<>();
+        HashMap<String, GroceryItem> groceryList = new HashMap<>();
+
+
+        //Asking user if they want to use the program.
+            System.out.println("Would you like to create a grocery list?");
+            String playGame = scanner.nextLine();
+            //If they want to use the program, use it, if not, break out of the loop.
+            if (playGame.equalsIgnoreCase("yes") || playGame.equalsIgnoreCase("y")) {
+                int value = 0;
+                while (value != 1) {
+
+                    // Asking user if they want to enter a grocery item
+                    System.out.println("Would you like to enter a grocery item?");
+                    String enterItemStatus = scanner.nextLine();
+
+                    if (enterItemStatus.equalsIgnoreCase("yes") || enterItemStatus.equalsIgnoreCase("y")) {
+                        System.out.println("Here is a list of all of the categories to choose from");
+                        for (int i = 0; i < categoryList.size(); i++) {
+                            System.out.println(categoryList.get(i));
+                        }
+                        System.out.println("Please choose one of the categories.");
+                        String userEnteredItemCategory = scanner.nextLine();
+                        System.out.println("Please enter the item name: ");
+                        String userEnteredItemName = scanner.nextLine();
+                        System.out.println("Please enter how many you would like to buy: ");
+                        int userEnteredItemAmount = scanner.nextInt();
+                        GroceryItem itemObject = new GroceryItem(userEnteredItemName, userEnteredItemAmount, userEnteredItemCategory);
+                        // Add this to the hashmap.
+                        groceryItemsList.add(userEnteredItemName);
+                        groceryList.put(userEnteredItemName, itemObject);
+                        scanner.nextLine();
+
+
+                        //At this point all of the parts that the user entered has been put into the hashmap.
+                    }
+                    else {
+                        System.out.println("Finalizing the list");
+                        System.out.println("Your entire List:");
+                        System.out.println("Name --- Quantity --- Category");
+                        System.out.println("----------------------");
+                        for (int i = 0; i < groceryList.size(); i++) {
+                            System.out.printf("Item %s: %s --- %s --- %s", (i + 1), groceryItemsList.get(i), groceryList.get(groceryItemsList.get(i)).getQuantity(), groceryList.get(groceryItemsList.get(i)).getCatagory());
+                            System.out.println();
+                        }
+
+                        value += 1;
+                    }
                 }
-
-                System.out.println("Please enter the name of the item: ");
-                String userItemName = scanner.nextLine();
-
-                System.out.println("Please enter the amount you'd like to add.");
-                int userItemAmount = scanner.nextInt();
-
-                GroceryItem userItem = new GroceryItem(userItemName, userItemType);
-                groceryList.put(userItem, userItemAmount);
-                System.out.printf("%s - %s", userItemName, userItemAmount);
             }
-        }
-        System.out.println();
         System.out.println("Thank you!");
     }
 }
